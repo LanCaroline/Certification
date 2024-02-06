bb12ca798f4c8239c0e3d29f3fead20bfabb50b9
LanCaroline <lana.de.sa@hotmail.com>
2024-02-06 17:27:15 -0300

    model by project

---
+++ src/main/java/com/projectlonrocketseat/certificationnlw/models/students/entities/CertificationEstudentsEntities.java
@@ -0,0 +1,10 @@
+package com.projectlonrocketseat.certificationnlw.models.students.entities;
+
+import java.util.UUID;
+
+public class CertificationEstudentsEntities {
+    private UUID id;
+    private UUID student_id;
+    private String technology;
+    private Double grate;
+}
---
+++ src/main/java/com/projectlonrocketseat/certificationnlw/models/students/entities/StudentEntity.java
@@ -0,0 +1,15 @@
+package com.projectlonrocketseat.certificationnlw.models.students.entities;
+
+import java.util.List;
+import java.util.UUID;
+
+public class StudentEntity {
+
+    private UUID id; //0186392b-03ca-4075-8674-3974f156f48b
+
+    private String email;
+
+    private List<CertificationEstudentsEntities> certificationEstudentsEntities;
+
+
+}
